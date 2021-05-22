caca3915bda6d8f42ab3486a033f742a1df142f8
Pierre-Adrien <pierreadrien.vasseur@gmail.com>
2021-05-22 18:31:58 +0200

    :tada: Initialize project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import * as SC from 'AppStyled'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SC.Container>
+      <SC.Header>
+
+      </SC.Header>
+    </SC.Container>
   );
 }
 
-export default App;
+export default App
